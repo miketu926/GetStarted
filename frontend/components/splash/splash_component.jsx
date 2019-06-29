50ebbd939e1b1cb0f7fb8d7937ad03643d1fb87d
Mike <miketu926@gmail.com>
2019-06-29 15:05:31 -0400

    Refactored splash component to utilize React hooks and Redux hooks. Used a set to randomize recommended and favorite projects since they were repeating before by using randomly pushed projects into an array

--- frontend/components/splash/splash_component.jsx
+++ frontend/components/splash/splash_component.jsx
@@ -1,138 +1,117 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { fetchAllProjects, fetchProject } from '../../actions/projects/project_actions';
 import { Link } from 'react-router-dom';
 import { Recommended, Favorites, Featured } from './splash_item';
 
-
-const msp = (state) => {
-  const { projects, users } = state.entities;
-
-  return ({
-    projects: projects,
-    users: users,
+function SplashComponent(props) {
+  const projects = useSelector(state => {
+    return state.entities.projects;
   });
-};
-
-const mdp = (dispatch) => {
-  return ({
-    fetchAllProjects: (searchTerm) => dispatch(fetchAllProjects(searchTerm)),
-    fetchProject: (id) => dispatch(fetchProject(id)),
+  const users = useSelector(state => {
+    return state.entities.users;
   });
-};
 
+  const dispatch = useDispatch();
 
-class SplashComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getFeatured = this.getFeatured.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     window.scrollTo(0, 0);
-    this.props.fetchAllProjects();
-  }
+    fetchAllProjects()(dispatch);
+  }, []);
 
-  numberWithCommas(x) {
+  const numberWithCommas = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
-  getFeatured(num = 1) {
-    let featured = [];
-    let allProjects = Object.values(this.props.projects);
-    let i = 0;
-    while (i < num) {
-      featured.push(allProjects[Math.floor(Math.random() * allProjects.length)]);
-      i++;
+  const getFeatured = (n = 1) => {
+    let allProjects = Object.values(projects);
+    let set = new Set();
+    while (set.size < n) {
+      set.add(allProjects[Math.floor(Math.random() * allProjects.length)]);
     }
-    return featured;
+    return [...set];
   }
 
-  handleSubmit(searchTerm) {
-    const that = this;
+  const handleSubmit = (searchTerm) => {
     if (searchTerm === "ALL_PROJECTS") {
-      this.props.fetchAllProjects(searchTerm)
+      fetchAllProjects(searchTerm)(dispatch)
         .then(() => {
-          that.props.history.push(`/search/all projects`);
+          props.history.push(`/search/all projects`);
           window.scrollTo(0, 0);
         });
     } else {
-      this.props.fetchAllProjects(searchTerm)
+      fetchAllProjects(searchTerm)(dispatch)
         .then(() => {
-          that.props.history.push(`/search/${searchTerm}`);
+          props.history.push(`/search/${searchTerm}`);
           window.scrollTo(0, 0);
         });
     }
   }
 
-  render() {
-    const { projects, users } = this.props;
+  if (!projects || !users || !getFeatured()[0]) {
+    return <div>Loading...</div>
+  }
 
-    if (!projects || !users || !this.getFeatured()[0]) {
-      return <div>Loading...</div>
-    }
-
-    const featured = this.getFeatured().map(project => {
-      return (
-        <Featured
-          key={project.id}
-          project={project}
-          user={users[project.user_id]}
-        />
-      );
-    })
-
-    const recommended = this.getFeatured(3).map(project => {
-      return (
-        <Recommended
-          numberWithCommas={this.numberWithCommas}
-          key={project.id}
-          project={project}
-          user={users[project.user_id]}
-        />
-      )
-    })
-
-    const favorites = this.getFeatured(4).map((project, idx) => {
-      return (
-        <Favorites
-          key={project.id}
-          project={project}
-          user={users[project.user_id]}
-          idx={idx}
-        />
-      )
-    })
-
+  const featured = getFeatured().map(project => {
     return (
-      <div className='flex flex-col'>
+      <Featured
+        key={project.id}
+        project={project}
+        user={users[project.user_id]}
+      />
+    );
+  })
 
-        <div className='categories-NAV flex row-wrap justify-center'>
-          <button onClick={() => { this.handleSubmit("Arts") }} className='hover cat'>Arts</button>
-          <button onClick={() => { this.handleSubmit("Design") }} className='hover cat'>Design</button>
-          <button onClick={() => { this.handleSubmit("Film") }} className='hover cat'>Film</button>
-          <button onClick={() => { this.handleSubmit("Games") }} className='hover cat'>Games</button>
-          <button onClick={() => { this.handleSubmit("Music") }} className='hover cat'>Music</button>
-          <button onClick={() => { this.handleSubmit("Photography") }} className='hover cat'>Photography</button>
-          <button onClick={() => { this.handleSubmit("Publishing") }} className='hover cat'>Publishing</button>
-          <button onClick={() => { this.handleSubmit("Technology") }} className='hover cat'>Technology</button>
+  const recommended = getFeatured(3).map(project => {
+    return (
+      <Recommended
+        numberWithCommas={numberWithCommas}
+        key={project.id}
+        project={project}
+        user={users[project.user_id]}
+      />
+    )
+  })
+
+  const favorites = getFeatured(4).map((project, idx) => {
+    return (
+      <Favorites
+        key={project.id}
+        project={project}
+        user={users[project.user_id]}
+        idx={idx}
+      />
+    )
+  })
+
+  return (
+    <div className='flex flex-col'>
+
+      <div className='categories-NAV flex row-wrap justify-center'>
+        <button onClick={() => handleSubmit("Arts")} className='hover cat'>Arts</button>
+        <button onClick={() => handleSubmit("Design")} className='hover cat'>Design</button>
+        <button onClick={() => handleSubmit("Film")} className='hover cat'>Film</button>
+        <button onClick={() => handleSubmit("Games")} className='hover cat'>Games</button>
+        <button onClick={() => handleSubmit("Music")} className='hover cat'>Music</button>
+        <button onClick={() => handleSubmit("Photography")} className='hover cat'>Photography</button>
+        <button onClick={() => handleSubmit("Publishing")} className='hover cat'>Publishing</button>
+        <button onClick={() => handleSubmit("Technology")} className='hover cat'>Technology</button>
+      </div>
+
+      <div className='flex row-wrap justify-center featured'>
+        {featured}
+        <div className='flex flex-col padding-lr margin-right-260'>
+          <h2 className='title-section padding-bot-20'>RECOMMENDED</h2>
+          {recommended}
+          <Link onClick={() => handleSubmit("ALL_PROJECTS")} className='view-more' to='/'>View more projects</Link>
         </div>
+      </div>
 
-        <div className='flex row-wrap justify-center featured'>
-          {featured}
-          <div className='flex flex-col padding-lr margin-right-260'>
-            <h2 className='title-section padding-bot-20'>RECOMMENDED</h2>
-            {recommended}
-            <Link onClick={() => { this.handleSubmit("ALL_PROJECTS") }} className='view-more' to='/'>View more projects</Link>
-          </div>
-        </div>
+      <div className='flex row-wrap justify-center bottom-border padding-bottom-60' >
+        {favorites}
+      </div>
 
-        <div className='flex row-wrap justify-center bottom-border padding-bottom-60' >
-          {favorites}
-        </div>
-
-        {/* <div className='flex row-wrap justify-center'>
+      {/* <div className='flex row-wrap justify-center'>
           <h2>MAKE 100 project row</h2>
         </div>
 
@@ -170,23 +149,17 @@ class SplashComponent extends React.Component {
           <h2>SUBSCRIBE TO KICKSTARTER</h2>
         </div> */}
 
-        <div className='cat-bot flex row-wrap justify-center'>
-          <button onClick={() => { this.handleSubmit("Arts") }} className='hover cat'>Arts</button>
-          <button onClick={() => { this.handleSubmit("Design") }} className='hover cat'>Design</button>
-          <button onClick={() => { this.handleSubmit("Film") }} className='hover cat'>Film</button>
-          <button onClick={() => { this.handleSubmit("Games") }} className='hover cat'>Games</button>
-          <button onClick={() => { this.handleSubmit("Music") }} className='hover cat'>Music</button>
-          <button onClick={() => { this.handleSubmit("Photography") }} className='hover cat'>Photography</button>
-          <button onClick={() => { this.handleSubmit("Publishing") }} className='hover cat'>Publishing</button>
-          <button onClick={() => { this.handleSubmit("Technology") }} className='hover cat'>Technology</button>
-        </div>
-
-      </div >
-    );
-
-  }
-
+      <div className='cat-bot flex row-wrap justify-center'>
+        <button onClick={() => handleSubmit("Arts")} className='hover cat'>Arts</button>
+        <button onClick={() => handleSubmit("Design")} className='hover cat'>Design</button>
+        <button onClick={() => handleSubmit("Film")} className='hover cat'>Film</button>
+        <button onClick={() => handleSubmit("Games")} className='hover cat'>Games</button>
+        <button onClick={() => handleSubmit("Music")} className='hover cat'>Music</button>
+        <button onClick={() => handleSubmit("Photography")} className='hover cat'>Photography</button>
+        <button onClick={() => handleSubmit("Publishing")} className='hover cat'>Publishing</button>
+        <button onClick={() => handleSubmit("Technology")} className='hover cat'>Technology</button>
+      </div>
+    </div >
+  );
 }
-
-
-export default connect(msp, mdp)(SplashComponent);
+export default SplashComponent;

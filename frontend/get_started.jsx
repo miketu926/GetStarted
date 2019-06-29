d9d3aaf1af47c7e71c1082de6da8a22e9961480e
Mike <miketu926@gmail.com>
2019-06-29 15:55:25 -0400

    Removed testing scripts and removed logger in preparation for production roll out

--- frontend/get_started.jsx
+++ frontend/get_started.jsx
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-// FOR TESTING IN DEVELOPMENT
-import { logout } from './actions/session/session_actions';
-import * as ALLAJAX from './util/projects_api_util';
-import * as PROJECTTHUNKS from './actions/projects/project_actions';
-// END TESTING - REMOVE IN PROD
-
 document.addEventListener('DOMContentLoaded', () => {
   let store = {};
   let preloadedState = {};
-  
+
   if (window.currentUser) {
     preloadedState = {
       session: {
@@ -28,30 +22,8 @@ document.addEventListener('DOMContentLoaded', () => {
   else {
     store = configureStore();
   }
-  
+
   const root = document.getElementById('root');
 
-  // FOR TESTING IN DEVELOPMENT
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.logout = logout;
-  window.fetchPROJECTS = PROJECTTHUNKS.fetchAllProjects;
-  window.fetchPROJECT = PROJECTTHUNKS.fetchProject;
-  window.createPROJECT = PROJECTTHUNKS.createProject;
-  window.deletePROJECT = PROJECTTHUNKS.deleteProject;
-  // END TESTING - REMOVE IN PROD
-  
   ReactDOM.render(<Root store={store} />, root);
 });
-
-
-
-// createUser, createSession, deleteSessiojn - AJAX
-  // window.createUser({email: "starwars1@gmail.com", password: "starwars1"});
-  // window.createSession({email: "starwars@gmail.com", password: "starwars"});
-  // deleteSession();
-
-// thunk createUser, createSession, logout
-  // dispatch(window.createUser({email: "111111@gmail.com", password: "111111"}));
-  // dispatch(window.login({email: "starwars@gmail.com", password: "starwars"}));
-  // dispatch(logout());
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import RootReducers from '../reducers/root_reducers';
 
 const configureStore = (preloadedState = {}) => {
-  return createStore(RootReducers, preloadedState, applyMiddleware(thunk, logger));
+  return createStore(RootReducers, preloadedState, applyMiddleware(thunk));
 };
 
 export default configureStore;
